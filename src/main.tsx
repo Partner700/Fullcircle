19eff944643a46773fe5db1f1734531c547dc10d
Partner700 <90774792+Partner700@users.noreply.github.com>
2026-07-28 07:40:38 +0100

    1

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { AuthProvider } from './context/AuthContext.tsx';
-<<<<<<< HEAD
 import { registerServiceWorker } from './registerServiceWorker.ts';
-=======
 import { AppErrorBoundary } from './components/AppErrorBoundary.tsx';
->>>>>>> 2efd354e1e3e325fe8d80f8c7607cb248656885f
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
