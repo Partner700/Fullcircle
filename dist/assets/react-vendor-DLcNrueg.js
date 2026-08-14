dab958e12cd4d23811a927b849a4c89b22f915a7
TNSorganization <tnsorganization@gmail.com>
2026-08-14 16:32:19 +0100

    Build app dropdown menu polish

--- dist/assets/react-vendor-DLcNrueg.js
+++ dist/assets/react-vendor-DLcNrueg.js
@@ -1,4 +1,4 @@
-import{r as _a}from"./icons-D42zhd8P.js";var Co={exports:{}},ve={},xo={exports:{}},_o={};/**
+import{r as _a}from"./icons-IWGp0M2H.js";var Co={exports:{}},ve={},xo={exports:{}},_o={};/**
  * @license React
  * scheduler.production.min.js
  *
